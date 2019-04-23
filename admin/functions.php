530cfce242b705654ebe22bdb709eb462250950c
gmetreveli <g.metreveli1993@gmail.com>
2019-04-23 09:18:04 +0400

    fixed errors in admin panel

--- admin/functions.php
+++ admin/functions.php
@@ -16,15 +16,15 @@ function insert_categories(){
 
     global $connection;
 
-    if (isset($_POST[submit])){
+    if (isset($_POST['submit'])){
 
         $cat_title = $_POST['cat_title'];
 
-        if ($cat_title = "" || empty($cat_title)){
+        if ($cat_title == "" || empty ($cat_title)){
             echo "This field should not be empty";
-        }else {
+        } else {
             $query = "INSERT INTO categories (cat_title) ";
-            $query .= "VALUE('$cat_title') ";
+            $query .= "VALUE ('$cat_title')";
             $create_category_query = mysqli_query($connection, $query);
             if (! $create_category_query){
                 die('QUERY FAILEd' . mysqli_error($connection));
@@ -64,7 +64,7 @@ function delete_categories(){
     if (isset($_GET['delete'])){
 
         $the_cat_id = $_GET['delete'];
-        $query = "DELETE FROM categories WHERE cat_is = {$the_cat_id}";
+        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
         $delete_query = mysqli_query($connection, $query);
 
         header("Location: categories.php");
